47f0b5d49be3066928731620f59d30213f5a6ac5
nazmisani <nazminouval@gmail.com>
2025-08-20 02:58:38 +0700

    feat: add category and customer routers to the main routing index

--- server/src/routers/index.route.ts
+++ server/src/routers/index.route.ts
@@ -4,6 +4,8 @@ import userRouter from "./user.route";
 import logRouter from "./log.route";
 import authRouter from "./auth.route";
 import reportRouter from "./report.route";
+import categoryRouter from "./category.route";
+import customerRoter from "./customer.route";
 
 import express from "express";
 import errorHandler from "../middlewares/errorHandler";
@@ -17,6 +19,8 @@ router.use(authentication);
 
 router.use("/users", userRouter);
 router.use("/products", ProductRouter);
+router.use("/customer", customerRoter);
+router.use("/category", categoryRouter);
 router.use("/transactions", transactionRouter);
 router.use("/logs", logRouter);
 router.use("/reports", reportRouter);
