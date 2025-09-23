44d6196dc05d4df6ce474c3b25856ef6e9396602
nazmisani <nazminouval@gmail.com>
2025-09-23 21:15:03 +0700

    fix: restore authorization middleware for user routes and ensure proper role access

--- server/src/routers/user.route.ts
+++ server/src/routers/user.route.ts
@@ -3,11 +3,13 @@ import UserController from "../controllers/user.controller";
 import { authorizeRole } from "../middlewares/authorization";
 const router = express.Router();
 
-router.use(authorizeRole("admin"));
-
-router.get("/", UserController.getUser);
-router.post("/", UserController.createUser);
-router.put("/:id", UserController.updateUser);
-router.delete("/:id", UserController.deleteUser);
-
+router.get("/", authorizeRole("admin"), UserController.getUser);
+router.get(
+  "/total",
+  authorizeRole("admin", "manager", "cashier"),
+  UserController.getTotalUser
+);
+router.post("/", authorizeRole("admin"), UserController.createUser);
+router.put("/:id", authorizeRole("admin"), UserController.updateUser);
+router.delete("/:id", authorizeRole("admin"), UserController.deleteUser);
 export default router;
